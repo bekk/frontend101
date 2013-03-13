ad7622717d88248e882e55f70b54da4ecd01a616
Helge Fredheim <helge.fredheim@bekk.no>
2013-03-13 10:18:56 +0100

    Fiksa litt kontinuitet mellom del 2 og del 3

--- kurs2/del2/oppgave/scripts/image_gallery.js
+++ kurs2/del2/oppgave/scripts/image_gallery.js
@@ -1,34 +1,44 @@
 /* MY IMAGE */
 
-var MyImage = function(item) 
-	// this.src_small = item.?.?.? (Bytt ut "_m" med "_s" i image.media.m)
-	// this.src_large = ? (Fjern "_m" i image.media.m)
-	// this.title = ?
-	// this.el = $('<img title="' + ?? + '" alt="' + ?? + '" class="image-small" src="' + ?? + '" />')
+var MyImage = function(item) {
+	// this.srcSmall = item.?. // (Bytt ut "_m" med "_s" i image.media.m)
+	// this.srcLarge = item.?.?... // (Fjern "_m" i image.media.m)
+	// this.title = item.title;
+	// this.largeImage = $('<img title="' + ? + '" alt="' + ? + '" class="image-large" src="' + ? + '" />');
+	// this.smallImage = $('<img title="' + ? + '" alt="' + ? + '" class="image-small" src="' + ? + '" />');
+};
+
+MyImage.prototype.init = function() {	
+	this.preload(); 
+}
+
+MyImage.prototype.preload = function() {
+	// Lite triks for å forhåndslaste et bilde inn i nettleseren, før det vises
+	var img = new Image();
+	img.src = this.srcLarge;
 };
 
 /* IMAGE GALLERY */
 
 var images = [];
-var stage = $('<div class="image-stage" />');
 
-var generate_flickr_url = function(query) {
+var generateFlickrUrl = function(query) {
 	var url = "http://api.flickr.com/services/feeds/photos_public.gne?lang=en-us&tags=" + query + "&tagmode=all&format=json&jsoncallback=?"
 	return url;
 };
 
-var create_image_objects = function(items) {
+var createImageObjects = function(items) {
 	// $(items).each(function() {
 	// 	// Her itererer vi over items, og lager et nytt bilde-objekt per item 
 	//	// Objektet lagrer vi dernest i det globale images-arrayet 
 	// });
 };
 
-var render_image_menu = function(number_of_images) {
+var renderThumbs = function(numberOfImages) {
 	var container = $('<div class="images-nav" />');
 
-	for(i = 0; i < number_of_images; i++) {
-		var image = images[i];
+	for(i = 0; i < numberOfImages; i++) {
+		// var image = images[i];
 		// For hvert MyImage, legg MyImage.el inn i container
 		// Tips: jQuery append
 	};
@@ -36,19 +46,13 @@ var render_image_menu = function(number_of_images) {
 	return container; 
 };
 
-var start_image_gallery = function(el) {
+var getImagesFor = function (query, $renderTo) {
 
-	// Tar i mot et dom-element med jQuery-funksjoner, f.eks.
-	// $('<div id="bildegalleri" />');
+	var numberOfImages = 8;
+	var url = generateFlickrUrl(query);
+	var $loadingMessage = $("<p class='loading'>Please wait while we load some images tagged with <strong>" + query + "</strong> from Flickr</p>");
 
-	var query = "beer";
-	var number_of_images = 8;
-
-	var url = generate_flickr_url(query);
-
-	var loading_message = $("<p class='loading'>Please wait while we load some images tagged with <strong>" + query + "</strong> from Flickr</p>");
-
-	el.html(loading_message);
+	$renderTo.html($loadingMessage);
 
 	$.ajax({
 		// dataType: "jsonp"
@@ -57,5 +61,17 @@ var start_image_gallery = function(el) {
 		// Se presentasjonen, eller google $.ajax :-)
 		// Tips: Begynn med console.log(respons) for å verifisere at du mottar data fra Flickr
 	});
+};
+
+/* APPLICATION SETUP */
+
+var startImageGallery = function(el) {
+
+	// Tar i mot et dom-element med jQuery-funksjoner, f.eks.
+	// $('<div id="bildegalleri" />');	
+
+	var $thumbsContainer = el.find("#thumbs");
+	var tag = "trondheim";
+	getImagesFor(tag, $thumbsContainer);
 
 };
--- kurs2/del3/oppgave/scripts/image_gallery.js
+++ kurs2/del3/oppgave/scripts/image_gallery.js
@@ -1,58 +1,66 @@
 /* MY IMAGE */
 
 var MyImage = function(item) {
-	this.src_small = item.media.m.replace("_m", "_s");
-	this.src_large = item.media.m.replace("_m", "");
+	this.srcSmall = item.media.m.replace("_m", "_s");
+	this.srcLarge = item.media.m.replace("_m", "");
 	this.title = item.title;
-	this.el = $('<img title="' + this.title + '" alt="' + this.title + '" class="image-small" src="' + this.src_small + '" />')
+	this.largeImage = $('<img title="' + this.title + '" alt="' + this.title + '" class="image-large" src="' + this.srcLarge + '" />');
+	this.smallImage = $('<img title="' + this.title + '" alt="' + this.title + '" class="image-small" src="' + this.srcSmall + '" />');
+};
+
+MyImage.prototype.init = function() {
+	this.preload(); 
+}
+
+MyImage.prototype.preload = function() {
+	var img = new Image();
+	img.src = this.srcLarge;
 };
 
 /* IMAGE GALLERY */
 
 var images = [];
 
-var generate_flickr_url = function(query) {
+var generateFlickrUrl = function(query) {
 	var url = "http://api.flickr.com/services/feeds/photos_public.gne?lang=en-us&tags=" + query + "&tagmode=all&format=json&jsoncallback=?"
 	return url;
 };
 
-var create_image_objects = function(items) {
+var createImageObjects = function(items) {
 	// Vi itererer over respons, og lager et nytt bilde-objekt per item 
-	images = []; // husk på å tøm lista for hver gang
 	$(items).each(function() {
 		var item = this;
 		var img = new MyImage(item); 
+		img.init(); 
 		images.push(img); // Vi legger til hvert bilde-objekt på slutten av arrayet 
 	});
 };
 
-var render_image_menu = function(number_of_images) {
+var renderThumbs = function(numberOfImages) {
 	var container = $('<div class="images-nav" />');
 
-	for(i = 0; i < number_of_images; i++) {
+	for(i = 0; i < numberOfImages; i++) {
 		var image = images[i];
-		container.append(image.el);
+		container.append(image.smallImage);
 	};
 
 	return container; 
 };
 
-var get_image_for = function (query, $renderTo) {
+var getImagesFor = function (query, $renderTo) {
 
-	var number_of_images = 8;
+	var numberOfImages = 8;
+	var url = generateFlickrUrl(query);
+	var $loadingMessage = $("<p class='loading'>Please wait while we load some images tagged with <strong>" + query + "</strong> from Flickr</p>");
 
-	var url = generate_flickr_url(query);
-
-	var loading_message = $("<p class='loading'>Please wait while we load some images tagged with <strong>" + query + "</strong> from Flickr</p>");
-
-	$renderTo.html(loading_message);
+	$renderTo.html($loadingMessage);
 
 	$.ajax({
 		dataType: "jsonp", 
 		url: url,
 		success: function(response) {
-			create_image_objects(response.items); 
-			var html = render_image_menu(number_of_images); 
+			createImageObjects(response.items); 
+			var html = renderThumbs(numberOfImages); 
 			$renderTo.html(html);
 		},
 		error: function() {
@@ -63,11 +71,23 @@ var get_image_for = function (query, $renderTo) {
 
 /* APPLICATION SETUP */
 
+var startImageGallery = function(el) {
 
-var start_image_gallery = function(el) {
+	// Tar i mot et dom-element med jQuery-funksjoner, f.eks.
+	// $('<div id="bildegalleri" />');	
 
-	var $inputField = el.find("input");
-	var $thumbs = el.find("#thumbs");
-	var $largeImage = el.find("#largeImage");
+	var $thumbsContainer = el.find("#thumbs");
+	// var $inputField = el.find("#tagSearch");
+	// var $largeImageContainer = el.find("#largeImage");
+
+	$inputField.on("keyup", function(event){
+		var keyCode = event.keyCode;
+		// Gjør et søk basert på det som står i input-feltet
+	};
+
+	$thumbsContainer.on("click", "img", function() {
+		var image = $this;
+		// Vis stort bilde i largeImageContainer ved klikk på thumbnail
+	}); 
 
 };
