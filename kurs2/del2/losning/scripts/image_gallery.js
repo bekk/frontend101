a735cffb5b5ecb5ca85bfd49478a3d1d9db02845
Kent Andersen <kentareandersen@gmail.com>
2014-03-09 22:59:54 +0100

    refakturerte løsningsforslaget

--- kurs2/del2/losning/scripts/image_gallery.js
+++ kurs2/del2/losning/scripts/image_gallery.js
@@ -1,83 +1,62 @@
-/* MY IMAGE */
-
-var MyImage = function(item) {
-	this.srcSmall = item.media.m.replace("_m", "_s");
-	this.srcLarge = item.media.m.replace("_m", "");
-	this.title = item.title;
-	this.largeImage = $('<img title="' + this.title + '" alt="' + this.title + '" class="image-large" src="' + this.srcLarge + '" />');
-	this.smallImage = $('<img title="' + this.title + '" alt="' + this.title + '" class="image-small" src="' + this.srcSmall + '" />');
-};
-
-MyImage.prototype.init = function() {
-	this.preload(); 
-}
-
-MyImage.prototype.preload = function() {
-	var img = new Image();
-	img.src = this.srcLarge;
-};
-
-/* IMAGE GALLERY */
-
-var images = [];
+// utilities
 
 var generateFlickrUrl = function(query) {
-	var url = "http://api.flickr.com/services/feeds/photos_public.gne?lang=en-us&tags=" + query + "&tagmode=all&format=json&jsoncallback=?"
-	return url;
+	return "http://api.flickr.com/services/feeds/photos_public.gne?lang=en-us&tags=" + query + "&tagmode=all&format=json&jsoncallback=?"
 };
 
-var createImageObjects = function(items) {
-	// Vi itererer over respons, og lager et nytt bilde-objekt per item 
-	$(items).each(function() {
-		var item = this;
-		var img = new MyImage(item); 
-		img.init(); 
-		images.push(img); // Vi legger til hvert bilde-objekt på slutten av arrayet 
-	});
-};
-
-var renderThumbs = function(numberOfImages) {
-	var container = $('<div class="images-nav" />');
-
-	for(i = 0; i < numberOfImages; i++) {
-		var image = images[i];
-		container.append(image.smallImage);
-	};
-
-	return container; 
-};
-
-var getImagesFor = function (query, $renderTo) {
-
-	var numberOfImages = 8;
-	var url = generateFlickrUrl(query);
-	var $loadingMessage = $("<p class='loading'>Please wait while we load some images tagged with <strong>" + query + "</strong> from Flickr</p>");
-
-	$renderTo.html($loadingMessage);
+var getDataFromFlickr = function(url, successCallback, errorCallback) {
 
 	$.ajax({
-		dataType: "jsonp", 
+		dataType: "jsonp",
 		url: url,
-		success: function(response) {
-			createImageObjects(response.items); 
-			var html = renderThumbs(numberOfImages); 
-			$renderTo.html(html);
-		},
-		error: function() {
-			$renderTo.html("Sorry, an error occured.");
-		}
+		success: successCallback,
+		error: errorCallback
+	});
+
+};
+
+
+// application
+
+var renderThumbItem = function(image) {
+	var url = image.media.m.replace("_m", "_s");
+	var title = image.title;
+
+	return '<img alt="' + title + '" src="' + url + '" />';
+};
+
+var renderThumbs = function(images, numberOfImages) {
+	var html = "";
+	var imagesToRender = (numberOfImages < images.length) ? numberOfImages : images.length;
+
+	for (var i = 0; i < imagesToRender; i++) {
+		html += renderThumbItem(images[i]);
+	};
+
+	return '<div id="thumbs">' + html + "</div>";
+};
+
+
+// Denne funksjonen kalles når siden er ferdig lastet
+var app = function() {
+	var tag = "trondheim";
+	var url = generateFlickrUrl(tag);
+	var $appElement = $("#image_gallery")
+
+
+	var loadingHtml = "<p class='loading'>Please wait while we load some images tagged with <strong>" + tag + "</strong> from Flickr</p>";
+
+	$appElement.html(loadingHtml);
+
+
+	getDataFromFlickr(url, function(response) {
+		var thumbsHtml = renderThumbs(response.items, 10);
+		$appElement.html(thumbsHtml);
+	}, function() {
+		alert("Ops! something went wrong");
 	});
 };
 
-/* APPLICATION SETUP */
+// startup
 
-var startImageGallery = function(el) {
-
-	// Tar i mot et dom-element med jQuery-funksjoner, f.eks.
-	// $('<div id="bildegalleri" />');	
-
-	var $thumbsContainer = el.find("#thumbs");
-	var tag = "trondheim";
-	getImagesFor(tag, $thumbsContainer);
-
-};
+$(document).ready(app);
