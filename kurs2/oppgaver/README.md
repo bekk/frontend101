167487551c7c6e9078312f1373b829f66932cc3d
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2014-03-10 09:00:53 +0100

    Fjerner jsbin-greiene.

--- kurs2/oppgaver/README.md
+++
@@ -1,9 +0,0 @@
-# Kjør oppgavene lokalt
-
-    sh start.sh
-
-# Har du endret en oppgave og vil lagre den?
-
-- Start jsbin
-- Fiks oppgaven
-- Add og commit jsbin.sqlite-fila
--- kurs2/oppgaver/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-npm install
-
-SQLITE_PATH=$(pwd)/jsbin.sqlite
-
-JSBIN_API_ALLOWANONYMOUSREADWRITE=true \
-  JSBIN_STORE_SQLITE_LOCATION=$SQLITE_PATH \
-  `npm bin`/jsbin
